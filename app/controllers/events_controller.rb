db8735188e7200554bf701e1fd2e0bcfe61c5e2c
Mike Rachman <mikerachman1@gmail.com>
2022-09-10 11:10:21 -0400

    Updated EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,15 +11,58 @@ class EventsController < ApplicationController
     @event = current_user.created_events.build(event_params)
 
     if @event.save
-      flash[:success] = "Your event was created"
-      redirect_to @event
+      redirect_to @event, notice: "Event Created"
     else
-      flash[:alert] = "Error"
+      flash.now[:notice] = 'Error'
       render :new
     end
   end
 
   def show
     @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event, notice: "Event Updated"
+    else 
+      flash.now[:notice] = 'Error'
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  def rsvp
+    @event = Event.find(params[:id])
+    
+    if @event.attendees.include?(current_user)
+      redirect_to @event, notice: "You are on list"
+    else
+      @event.attendees << current_user
+      redirect_to @event
+    end
+  end
+
+  def cancel_rsvp
+    @event = Event.find(params[:id])
+    @event.attendees.delete(current_user)
+    redirect_to @event, notice: "You were removed from list"
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :location)
   end
 end
