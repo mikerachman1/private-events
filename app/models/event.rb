fc1a2fdfa50a7106e85e0fa5def273a98cd19a64
Mike Rachman <mikerachman1@gmail.com>
2022-09-10 12:07:34 -0400

    updated Event model to scope :past and :future

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,16 @@ class Event < ApplicationRecord
   has_many :rsvps, foreign_key: :attended_event_id
 
   has_many :attendees, through: :rsvps
+
+  # def self.past
+  #   self.where('date < ?', Date.today).order(:date)
+  # end
+
+  # def self.future
+  #   self.where('date >= ?', Date.today).order(:date)
+  # end 
+
+  scope :past, -> { where('date < ?', Date.today).order(:date) }
+  scope :future, -> { where('date >= ?', Date.today).order(:date) }
+  
 end
